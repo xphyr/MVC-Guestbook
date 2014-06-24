e1a9b64225ea29a3ef9765bd5c79cbf1bee4150d
rr222cy <rr222cy@student.lnu.se>
2014-06-24 13:31:58 +0200

    Read and Create functionality added

---
+++ GeekGuestbook2/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GeekGuestbook2.Models;
+
+namespace GeekGuestbook2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private GeekGuestbookEntities _entities = new GeekGuestbookEntities();
+
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            var model = _entities.Messages.ToList().OrderByDescending(m => m.MessageId);
+
+            return View(model);
+        }
+
+        //
+        // GET: /Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Create
+        [HttpPost]
+        public ActionResult Create(Message message)
+        {
+            message.Created = DateTime.Now;
+
+            _entities.Messages.Add(message);
+            _entities.SaveChanges();
+
+            return RedirectToAction("");
+        }
+	}
+}
